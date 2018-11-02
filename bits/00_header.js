9e0c378bef406d87d52f09660bea553692b49252
Sol <sdemuth@fairbanksllc.com>
2018-11-02 09:05:31 -0500

    update header

--- bits/00_header.js
+++ bits/00_header.js
@@ -1,4 +1,5 @@
 /* xlsx.js (C) 2013-2020 SheetJS -- http://sheetjs.com */
+/* branch to allow row heights and styling */
 /* vim: set ts=2: */
 /*jshint -W041 */
 /*jshint funcscope:true, eqnull:true */
