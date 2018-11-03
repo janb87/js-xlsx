a387d4f28584f4c5b1877b6ee65e7122793257d6
Sol <sdemuth@fairbanksllc.com>
2018-11-02 19:18:13 -0500

    should work now
    I hope

--- bits/00_header.js
+++ bits/00_header.js
@@ -1,6 +1,6 @@
 /* xlsx.js (C) 2013-2020 SheetJS -- http://sheetjs.com */
 /* branch to allow row heights and styling */
-/* asdf  */
+/* asdf   sadfsdaasdf */
 /* Sol DeMuth 2018 sledmouth@gmail.com */
 var XLSX = {};
 (function make_xlsx(XLSX){
