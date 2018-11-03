984b7776b62ab5e62d917ed349c646d1129daf5f
Sol <sdemuth@fairbanksllc.com>
2018-11-02 19:19:27 -0500

    cleanup
    cleanup everybody

--- bits/00_header.js
+++ bits/00_header.js
@@ -1,6 +1,5 @@
 /* xlsx.js (C) 2013-2020 SheetJS -- http://sheetjs.com */
 /* branch to allow row heights and styling */
-/* asdf   sadfsdaasdf */
 /* Sol DeMuth 2018 sledmouth@gmail.com */
 var XLSX = {};
 (function make_xlsx(XLSX){
