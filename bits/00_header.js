b95286be6ef601b9b8395693777f436932abbcf9
Sol <sdemuth@fairbanksllc.com>
2018-11-02 19:21:36 -0500

    capitalize
    Branch

--- bits/00_header.js
+++ bits/00_header.js
@@ -1,5 +1,5 @@
 /* xlsx.js (C) 2013-2020 SheetJS -- http://sheetjs.com */
-/* branch to allow row heights and styling */
+/* Branch to allow row heights and styling */
 /* Sol DeMuth 2018 sledmouth@gmail.com */
 var XLSX = {};
 (function make_xlsx(XLSX){
