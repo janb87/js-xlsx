82fda9b2e2d98f0e617685a2766327730d1fe3e7
Sol <sdemuth@fairbanksllc.com>
2018-11-02 19:14:09 -0500

    cleanup header
    More webhook testing

--- bits/00_header.js
+++ bits/00_header.js
@@ -1,8 +1,5 @@
 /* xlsx.js (C) 2013-2020 SheetJS -- http://sheetjs.com */
 /* branch to allow row heights and styling */
 /* Sol DeMuth 2018 sledmouth@gmail.com */
-/* vim: set ts=2: */
-/*jshint -W041 */
-/*jshint funcscope:true, eqnull:true */
 var XLSX = {};
 (function make_xlsx(XLSX){
