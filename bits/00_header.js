609523b686c197c375958aba2bc1ecfa603e50c5
Sol <sdemuth@fairbanksllc.com>
2018-11-02 19:16:21 -0500

    more
    testing
    ...

--- bits/00_header.js
+++ bits/00_header.js
@@ -1,5 +1,6 @@
 /* xlsx.js (C) 2013-2020 SheetJS -- http://sheetjs.com */
 /* branch to allow row heights and styling */
+/* */
 /* Sol DeMuth 2018 sledmouth@gmail.com */
 var XLSX = {};
 (function make_xlsx(XLSX){
