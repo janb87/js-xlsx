083bfeabc5eba1c8532094d3cc1edfec6d606db0
Sol <sdemuth@fairbanksllc.com>
2018-11-05 08:56:11 -0600

    Test Moo version of GitHubWebhookRequest
    Should trigger email and chop off this subject.

--- bits/00_header.js
+++ bits/00_header.js
@@ -1,6 +1,6 @@
 /* xlsx.js (C) 2013-2020 SheetJS -- http://sheetjs.com */
 /* Branch to allow row heights and styling! */
-/* more tests  */
+/* more tests!!!  */
 /* Sol DeMuth 2018 sledmouth@gmail.com */
 var XLSX = {};
 (function make_xlsx(XLSX){
