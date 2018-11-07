2a1008075b5d4cb1c8d62f2c171672390de8e723
Sol DeMuth <sdemuth@fairbanksllc.com>
2018-11-07 17:39:29 -0600

    this is a test of github webhooks!

--- bits/00_header.js
+++ bits/00_header.js
@@ -1,6 +1,4 @@
 /* xlsx.js (C) 2013-2015 SheetJS -- http://sheetjs.com */
-/* vim: set ts=2: */
-/*jshint -W041 */
-/*jshint funcscope:true, eqnull:true */
+/* style and cell heights */
 var XLSX = {};
 (function make_xlsx(XLSX){
